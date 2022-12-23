85e297d62783cfe30b000da4204764c52cc4dc32
Voff4eg1905 <106484238+Voff4eg1905@users.noreply.github.com>
2022-12-23 13:42:50 -0500

    8 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const formEl = document.querySelector(".login-form");
+const emailInputEl = document.querySelector('input[name="email"]');
+const passInputEl = document.querySelector('input[type = "password"]');
 
+const onSubmit = (event) => {
+    event.preventDefault();
+    const {elements: {email, password}} = event.currentTarget;
+    if (email.value === "" || password.value === "") {
+        alert("All fields must be filled");
+    }
+
+    const userData = {
+        "email": email.value,
+        "password": password.value,
+    }
+    console.log(userData);
+  
+    function reset () {
+        email.value = "";
+        password.value = "";
+    }
+    reset ();
+    
+}
+formEl.addEventListener("submit", onSubmit);
