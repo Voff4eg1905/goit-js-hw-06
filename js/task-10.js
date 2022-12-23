aa84857ed08430da74a15d896d5c48653c063d7f
Voff4eg1905 <106484238+Voff4eg1905@users.noreply.github.com>
2022-12-23 14:18:31 -0500

    10 start

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,9 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const inputEl = document.querySelector("input");
+const createButtonEl = document.querySelector("button[data-create]");
+const resetButtonEl = document.querySelector("button[data-destroy]")
+console.log(resetButtonEl);
+
