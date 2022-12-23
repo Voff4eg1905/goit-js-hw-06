6b865a28e2a4f467dcf97321df550c62a1409278
Voff4eg1905 <106484238+Voff4eg1905@users.noreply.github.com>
2022-12-23 14:08:41 -0500

    9 done

--- js/task-08.js
+++ js/task-08.js
@@ -3,23 +3,28 @@ const emailInputEl = document.querySelector('input[name="email"]');
 const passInputEl = document.querySelector('input[type = "password"]');
 
 const onSubmit = (event) => {
+    
     event.preventDefault();
+    
     const {elements: {email, password}} = event.currentTarget;
-    if (email.value === "" || password.value === "") {
-        alert("All fields must be filled");
-    }
-
+    
     const userData = {
         "email": email.value,
         "password": password.value,
     }
-    console.log(userData);
-  
+
+
+    if (email.value === "" || password.value === "") {
+        alert("All fields must be filled");
+    } else {
+        console.log(userData);
+        reset ();
+    }
+
     function reset () {
         email.value = "";
         password.value = "";
     }
-    reset ();
-    
+ 
 }
 formEl.addEventListener("submit", onSubmit);
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const buttonEl = document.querySelector(".change-color");
+const spanEl = document.querySelector(".color");
+
+const onButtonClick = () => {
+  let colorForBody = getRandomHexColor();
+  spanEl.textContent = colorForBody;
+  document.body.style.backgroundColor = colorForBody;
+}
+buttonEl.addEventListener("click", onButtonClick);
