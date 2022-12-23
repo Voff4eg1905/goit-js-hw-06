d62e7ca06c6f8d86c26a385455323da95f758ec3
Voff4eg1905 <106484238+Voff4eg1905@users.noreply.github.com>
2022-12-23 12:55:07 -0500

    6 completed

--- js/task-06.js
+++ js/task-06.js
@@ -1,2 +1,34 @@
 const inputEl = document.querySelector("#validation-input");
-console.log(inputEl);
+
+const onInputElBlur = (event) => {
+  event.currentTarget.classList.remove("valid");
+  event.currentTarget.classList.remove("invalid");
+
+  /* Якщо не додаю рядки 6 і 7 і потім декілька разів змінюю кількість символів в інпуті без перезавантаження сторінки, 
+  воно залишає раніше доданий клас і тоді некоректно працює. Як правильно зробити? */
+
+ 
+  if (
+    event.currentTarget.value.length ===
+    Number(event.currentTarget.getAttribute("data-length"))
+  ) {
+    event.currentTarget.classList.add("valid");
+  } else {
+    event.currentTarget.classList.add("invalid");
+  }
+  
+};
+
+inputEl.addEventListener("blur", onInputElBlur);
+
+
+
+//? Another option
+
+// event.currentTarget.classList.add("invalid");
+// if (
+//   event.currentTarget.value.length ===
+//   Number(event.currentTarget.getAttribute("data-length"))
+// ) {
+//   event.currentTarget.classList.replace("invalid", "valid");
+// }
