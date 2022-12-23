92c7553310fa72d858e5b263b26b73ad691aabb6
Voff4eg1905 <106484238+Voff4eg1905@users.noreply.github.com>
2022-12-23 14:26:17 -0500

    10 start

--- js/task-10.js
+++ js/task-10.js
@@ -5,5 +5,17 @@ function getRandomHexColor() {
 const inputEl = document.querySelector("input");
 const createButtonEl = document.querySelector("button[data-create]");
 const resetButtonEl = document.querySelector("button[data-destroy]")
-console.log(resetButtonEl);
 
+
+
+const onCreateButtonClick = event => {
+  let numberOfCopies = inputEl.value;
+  createBoxes(numberOfCopies);
+}
+
+function createBoxes (number) {
+  console.log(number);
+}
+
+
+createButtonEl.addEventListener("click", onCreateButtonClick);
