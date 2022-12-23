81ca769866838e28fa52df4287f06b1bbbcc80ce
Voff4eg1905 <106484238+Voff4eg1905@users.noreply.github.com>
2022-12-23 14:33:18 -0500

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -10,11 +10,19 @@ const resetButtonEl = document.querySelector("button[data-destroy]")
 
 const onCreateButtonClick = event => {
   let numberOfCopies = inputEl.value;
+  if (numberOfCopies <= 0) {
+    alert("Need enter number that is bigger than 0");
+    return
+  }
+  
   createBoxes(numberOfCopies);
 }
 
 function createBoxes (number) {
   console.log(number);
+ for (let i = 0; i < number; i +=1) {
+  console.log(i);
+ }
 }
 
 
